af7d18ce685faccb7cf679b5b6643abeda89ee85
hyewon <hyewon@192.168.0.6>
2019-05-20 21:26:42 +0900

    Print calendar according to the month I input

--- src/Exercise/Calendar/Calendar.java
+++ src/Exercise/Calendar/Calendar.java
@@ -1,6 +1,5 @@
 package Exercise.Calendar;
 
-import java.util.Scanner;
 
 public class Calendar {
 
@@ -15,17 +14,26 @@ public class Calendar {
 			System.out.println("1~12까지 입력해주세요.");
 			return;
 		} else {
-			System.out.println("일  월  화  수  목  금");
-			System.out.println("-------------------");
-			System.out.println("1  2  3  4  5  6  7");
-			System.out.println("8  9  10 11 12 13 14");
-			System.out.println("15 16 17 18 19 20 21");
-			System.out.println("22 23 24 25 26 27 28");
-			if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-				System.out.println("29 30 31");
-			} else if (month == 4 || month == 6 || month == 9 || month == 11) {
-				System.out.println("29 30");
+			int max = GetMaxDaysOfMonth(month);
+			System.out.printf("   %3d월\n",month);
+			System.out.println("  일  월  화  수  목  금");
+			System.out.println("-----------------------");
+			for(int i = 1; i <= max; i++) {
+				System.out.printf("%3d", i);
+				if(i%7==0) {
+					System.out.println();
+				}
 			}
+				
+//			System.out.println("1  2  3  4  5  6  7");
+//			System.out.println("8  9  10 11 12 13 14");
+//			System.out.println("15 16 17 18 19 20 21");
+//			System.out.println("22 23 24 25 26 27 28");
+//			if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+//				System.out.println("29 30 31");
+//			} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+//				System.out.println("29 30");
+//			}
 		}
 	}
 
